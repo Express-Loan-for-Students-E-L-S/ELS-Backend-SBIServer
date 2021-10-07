1773d5524102493a3e0bbbe8bf2bb37bb2cfccef
Akshay Mali <akshaybmali02@gmail.com>
2021-10-08 00:57:08 +0530

    changes in grpc proto file

--- getUserData.js
+++ getUserData.js
@@ -1,10 +1,30 @@
 const client = require('./client')
 
 let phno = {
-    phoneNum: "02223615184"
+    phoneNum: "9845317234"
 }
 
-client.getUserBankInfo(phno, (error, userBankDetails) => {
+
+
+// client.GetConnectedBanks(phno, (error, userBankDetails) => {
+//     if (!error) {
+//         console.log('successfully fetch List userBankDetails')
+//         console.log(userBankDetails)
+//     } else {
+//         console.log(error)
+//     }
+// })
+
+// client.GetUserBankInfo({id: '6154429765cb7aa5e2fb55a8'}, (error, userBankDetails) => {
+//     if (!error) {
+//         console.log('successfully fetch List userBankDetails')
+//         console.log(userBankDetails)
+//     } else {
+//         console.log(error)
+//     }
+// })
+
+client.requestLoanOptions({fname: 'akshay', lname: 'mali'}, (error, userBankDetails) => {
     if (!error) {
         console.log('successfully fetch List userBankDetails')
         console.log(userBankDetails)
--- index.js
+++ index.js
@@ -25,60 +25,160 @@ const bankProto = grpc.loadPackageDefinition(packageDefinition);
 
 const server = new grpc.Server()
 server.addService(bankProto.BankService.service, {
-    getUserBankInfo: (call, callback) => {
+
+    getConnectedBanks: (call, callback) => {
         let phno = call.request.phoneNum;
+        BankDetail.find({
+            phoneNum: phno
+        })
+            .exec((err, userBankDetails) => {
+                if (err) {
+                    callback({
+                        code: grpc.status.NOT_FOUND,
+                        details: err.toString()
+                    })
+                }
+                if (userBankDetails) {
+
+                    // console.log(userBankDetails);
+
+                    let arr = []
+                    for (let i = 0; i < userBankDetails.length; i++) {
+                        arr.push({
+                            id: userBankDetails[i]._id.toString(),
+                            bankName: userBankDetails[i].bankStatement,
+                            accountNum: userBankDetails[i].accountNum,
+                            accountType: userBankDetails[i].accountType,
+                            ifscCode: userBankDetails[i].ifscCode
+                        })
+                    }
+                    console.log(arr);
+                    callback(null, {connectedBankAccounts: arr})
+                } else {
+                    callback({
+                        code: grpc.status.NOT_FOUND,
+                        details: "Not found"
+                    })
+                }
+            })
+    },
+
+    getUserBankInfo: (call, callback) => {
+        let id = call.request.id;
         BankDetail.findOne({
-            phoneNum : phno
+            _id: id
         })
-        .exec((err, userBankDetails) => {
-            if (err) {
-                callback({
-                    code: grpc.status.NOT_FOUND,
-                    details: err.toString()
-                })
-            }
-            if(userBankDetails) {
-                callback(null, {
-                    fname: userBankDetails.fname,
-                    lname: userBankDetails.lname,
-                    gender: userBankDetails.gender,
-                    address: userBankDetails.address,
-                    phoneNum: userBankDetails.phoneNum,
-                    dob: userBankDetails.dob,
-                    adharNum: userBankDetails.adharNum,
-                    panNum: userBankDetails.panNum,
-                    accountNum: userBankDetails.accountNum,
-                    ifscCode: userBankDetails.ifscCode,
-                    accountType: userBankDetails.accountType,
-                    bankStatement: userBankDetails.bankStatement
-                })
-            } else {
-                callback({
-                    code: grpc.status.NOT_FOUND,
-                    details: "Not found"
-                })
-            }
-        })
+            .exec((err, userBankDetails) => {
+                if (err) {
+                    callback({
+                        code: grpc.status.NOT_FOUND,
+                        details: err.toString()
+                    })
+                }
+                if (userBankDetails) {
+                    callback(null, {
+                        id: userBankDetails._id.toString(),
+                        fname: userBankDetails.fname,
+                        lname: userBankDetails.lname,
+                        gender: userBankDetails.gender,
+                        address: userBankDetails.address,
+                        phoneNum: userBankDetails.phoneNum,
+                        dob: userBankDetails.dob,
+                        fatherName: userBankDetails.fatherName || '',
+                        motherName: userBankDetails.motherName || '',
+                        adharNum: userBankDetails.adharNum,
+                        panNum: userBankDetails.panNum,
+                        accountNum: userBankDetails.accountNum,
+                        ifscCode: userBankDetails.ifscCode,
+                        accountType: userBankDetails.accountType,
+                        bankName: userBankDetails.bankStatement
+                    })
+                } else {
+                    callback({
+                        code: grpc.status.NOT_FOUND,
+                        details: "Not found"
+                    })
+                }
+            })
     },
 
     requestLoanOptions: (call, callback) => {
         callback(null, {
             loanOptions: [
                 {
+                    id: '1',
+                    bankName: 'State Bank of India',
+                    maxAmount: 1000000,
+                    duration: 5,
+                    interestRate: 8.9,
+                    colatral: 500000
+                },
+                {
+                    id: '2',
+                    bankName: 'Bank of India',
+                    maxAmount: 1100000,
+                    duration: 5,
+                    interestRate: 7.4,
+                    colatral: 700000
+                },
+                {
+                    id: '3',
+                    bankName: 'Axis Bank',
+                    maxAmount: 1400000,
+                    duration: 6,
+                    interestRate: 9.5,
+                    colatral: 600000
+                },
+                {
+                    id: '4',
+                    bankName: 'HDFC Bank',
+                    maxAmount: 1250000,
+                    duration: 6,
+                    interestRate: 6.6,
+                    colatral: 1100000
+                },
+                {
+                    id: '5',
+                    bankName: 'ICICI Bank',
+                    maxAmount: 1200000,
+                    duration: 4,
+                    interestRate: 10,
+                    colatral: 600000
+                },
+                {
+                    id: '6',
+                    bankName: 'Bank of Baroda',
+                    maxAmount: 2000000,
+                    duration: 7,
+                    interestRate: 7.4,
+                    colatral: 1500000
+                },
+                {
+                    id: '7',
+                    bankName: 'Yes Bank',
+                    maxAmount: 1200000,
+                    duration: 4,
+                    interestRate: 13.6,
+                    colatral: 800000
+                },
+                {
+                    id: '8',
                     bankName: "State Bank of India",
                     maxAmount: 1000000,
                     rateOfInterest: 5,
                     duration: 5,
-                    collateral: 1000000
+                    collateral: 700000
                 },
                 {
-                    bankName: "ICICI",
+                    id: '9',
+                    bankName: "ICICI Bank",
                     maxAmount: 1200000,
                     rateOfInterest: 8,
                     duration: 7,
-                    collateral: 1600000
+                    collateral: 460000
                 },
                 {
+                    id: '10',
                     bankName: "Bank of Baroda",
                     maxAmount: 800000,
                     rateOfInterest: 5,
@@ -86,7 +186,8 @@ server.addService(bankProto.BankService.service, {
                     collateral: 500000
                 },
                 {
-                    bankName: "HDFC",
+                    id: '11',
+                    bankName: "HDFC Bank",
                     maxAmount: 1150000,
                     rateOfInterest: 6,
                     duration: 4,
@@ -98,11 +199,6 @@ server.addService(bankProto.BankService.service, {
 
 })
 
-// string bankName = 1;
-//     int32 maxAmount = 2;
-//     float rateOfInterest = 3;
-//     int32 duration = 4;
-//     int32 collateral = 5;
 server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure())
 console.log('Bank Server running at http://127.0.0.1:50051')
 server.start()
